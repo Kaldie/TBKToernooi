1099fc899c794442d905705c90ac2b4b94901386
Ruud Cools <ruudcools@gmail.com>
2018-08-23 20:23:02 +0200

    reverted fu up

---
+++ client/landing.js
@@ -0,0 +1,20 @@
+import {Template} from 'meteor/templating';
+import {ReactiveVar} from 'meteor/reactive-var';
+
+import './landing.html'
+
+Template.landing.oncreate( function() {
+    this.counter = new ReactiveVar(0);
+})
+
+Template.landing.helpers({
+    counter() {
+	return Template.instance.counter.get();
+    },
+})
+
+Template.landing.events({
+    'click button'(event, instance) {
+	instance.counter.set(instance.counter.get() + 1)
+    }
+})
