cd657da645b070911b71fdf7cfb9d6cee31c7aaa
Ruud Cools <ruudcools@gmail.com>
2018-09-01 21:58:21 +0200

    intro iron router

--- client/main.js
+++ client/main.js
@@ -1,2 +1,12 @@
 import '../imports/startup/accounts-config.js';
 import './landing.js'
+import './bitch.html'
+
+Router.configure({
+    layoutTemplate: 'landing'
+})
+
+Route.route('/', function() {
+    this.layout('landing')
+    this.render('bitch', {to: "main"});
+})
