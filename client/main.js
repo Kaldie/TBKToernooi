352c7f36453035f589ddaba60626d0a7cb3255fb
DBergmans <davebergm@hotmail.nl>
2018-09-01 22:37:41 +0200

    Created basic admin portal

--- client/main.js
+++ client/main.js
@@ -1,6 +1,11 @@
 import '../imports/startup/accounts-config.js';
 import './landing.js'
 import './example.html'
+import './admin.html'
+import './pools.html'
+import './rounds.html'
+import './scores.html'
+import './teams.html'
 
 Router.configure({
     layoutTemplate: 'landing'
@@ -10,3 +15,24 @@ Router.route('/', function() {
     this.layout('landing')
     this.render('example', {to: 'main'});
 })
+
+Router.route('/admin', function() {
+    this.layout('landing');
+    this.render('admin', {to: 'main'});
+});
+Router.route('/admin/pools', function() {
+    this.layout('landing');
+    this.render('pools', {to: 'main'})
+});
+Router.route('/admin/rounds', function() {
+    this.layout('landing');
+    this.render('rounds', {to: 'main'})
+});
+Router.route('/admin/scores', function() {
+    this.layout('landing');
+    this.render('scores', {to: 'main'})
+});
+Router.route('/admin/teams', function() {
+    this.layout('landing');
+    this.render('teams', {to: 'main'})
+});
