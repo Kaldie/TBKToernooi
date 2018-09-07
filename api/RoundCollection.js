55c63ceef0d329c1d2311023eb61e5781172cd93
Ruud Cools <ruudcools@gmail.com>
2018-09-07 21:33:02 +0200

    moved stuff in their own folder for strucutere

--- api/RoundCollection.js
+++ api/RoundCollection.js
@@ -1,7 +1,50 @@
-import { _ } from 'underscore'
+import { _ } from '../../../.cache/typescript/2.9/node_modules/@types/underscore'
+
+
+export class RoundResult{
+    constructor(teamName1, teamName2, team1Points, team2Points) {
+        this.team1 = teamName1
+        this.team2 = teamName2
+        if (team1Points || team2Points) {
+            this.hasResults = true
+        } else {
+            this.hasResults = false
+        }
+        this.team1Points = team1Points || 0
+        this.team2Points = team2Points || 0
+    }
+}
 
 export class Rounds {
     constructor(doc) {
 	return _.extend(doc)
+    }
+
+    setResult(resultObject) {
+        check(resultObject,{
+            roundNumber: int,
+            team1: String,
+            team2: String,
+            team1Points: int,
+            team2Points: int
+        })
+        if (team1Points < 0  || team2Points < 0) {
+            throw RangeError("Points should be positive or 0")
+        }
+        if (team1 !== this.team1 || team2 !== this.team2) {
+            throw ReferenceError("Team names do not corrispond to this round result")
+        }
+
+        this.hasResults = true
+
+        this.team1Points = team1Points
+        this.team2Points = this.team2Points   
+    }
+
+    getResults() {
+        if (!this.hasResults) {
+            return undefined
+        }
+        return this
     }
 }
--- client/landing.js
+++ client/landing.js
@@ -4,6 +4,7 @@ import {ReactiveVar} from 'meteor/reactive-var';
 import './landing.html'
 import './imageColumn.js'
 import './landing.css'
+import { Meteor } from 'meteor/meteor';
 
 Template.landing.onCreated( function() {
 })
@@ -12,6 +13,9 @@ Template.landing.helpers({
     counter() {
 	return Template.instance.counter.get();
     },
+    year() {
+        return Meteor.settings.year || 2018
+    }
 })
 
 Template.landing.events({
